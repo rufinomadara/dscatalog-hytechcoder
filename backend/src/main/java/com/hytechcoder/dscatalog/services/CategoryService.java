2b3285ab0566d55c425ee16494f5b3452c4f4308
= <antonio.rufino1802@gmail.com>
2022-09-10 16:37:34 -0300

    Updating Category

--- backend/src/main/java/com/hytechcoder/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/hytechcoder/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.hytechcoder.dscatalog.dto.CategoryDTO;
 import com.hytechcoder.dscatalog.entities.Category;
 import com.hytechcoder.dscatalog.repositories.CategoryRepository;
-import com.hytechcoder.dscatalog.services.exceptions.EntityNotFoundException;
+import com.hytechcoder.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -36,7 +38,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
@@ -46,6 +48,19 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		Category entity = repository.getOne(id);
+		
+		try {
+			entity.setName(dto.getName());
+			entity = repository.save(entity);	
+			return new CategoryDTO(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}	
 	}
 
 
--- backend/src/main/java/com/hytechcoder/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/hytechcoder/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,9 @@
 package com.hytechcoder.dscatalog.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
 		
-		public EntityNotFoundException(String smg) {
+		public ResourceNotFoundException(String smg) {
 			super(smg);
 		}
 }
