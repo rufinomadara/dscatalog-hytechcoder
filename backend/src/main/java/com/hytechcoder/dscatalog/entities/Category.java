3137a14b907c595adae8c3f255b79db1c7e253c6
= <antonio.rufino1802@gmail.com>
2022-09-10 21:06:21 -0300

    Auditing data

--- backend/src/main/java/com/hytechcoder/dscatalog/entities/Category.java
+++ backend/src/main/java/com/hytechcoder/dscatalog/entities/Category.java
@@ -1,12 +1,16 @@
 package com.hytechcoder.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +23,13 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+//	Armazena no padrão a data no formato UTC
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Category() {
 	}
@@ -43,10 +54,34 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category(name) values ('Books');
-INSERT INTO tb_category(name) values ('Eletronics');
-INSERT INTO tb_category(name) values ('Computers');
+INSERT INTO tb_category(name, created_At) values ('Books', NOW());
+INSERT INTO tb_category(name, created_At) values ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_At) values ('Computers', NOW());
