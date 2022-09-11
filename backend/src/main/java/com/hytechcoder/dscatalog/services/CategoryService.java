664cee5a58ce5a474ea98d09c912d5732b3e00cc
= <antonio.rufino1802@gmail.com>
2022-09-11 20:49:34 -0300

    Refactor Pageable

--- backend/src/main/java/com/hytechcoder/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/hytechcoder/dscatalog/services/CategoryService.java
@@ -1,7 +1,6 @@
 package com.hytechcoder.dscatalog.services;
 
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -9,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,8 +26,8 @@ public class CategoryService {
 	
 //	Método para retornar a lista Paginada de todas as categorias
 	@Transactional(readOnly = true)
-	public Page<CategoryDTO> findAllPaged(PageRequest pageResquest){
-		Page<Category> list = repository.findAll(pageResquest);
+	public Page<CategoryDTO> findAllPaged(Pageable pageable){
+		Page<Category> list = repository.findAll(pageable);
 		return list.map(x -> new CategoryDTO(x));
 	}
 
