a052ba98867bb0e180e27095c65ca27b230e4380
= <antonio.rufino1802@gmail.com>
2022-09-19 10:15:10 -0300

    created entity User and Role

--- backend/src/main/java/com/hytechcoder/dscatalog/entities/Category.java
+++ backend/src/main/java/com/hytechcoder/dscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.hytechcoder.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -30,6 +33,9 @@ public class Category implements Serializable{
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
@@ -67,6 +73,10 @@ public class Category implements Serializable{
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
